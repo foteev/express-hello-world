5ffa8fee4fdbd711b3bce98647e9ab7f108d3b5c
vivi196 <foteev@gmail.com>
2023-02-27 19:18:08 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import authRouter from './routers/authentication-router.js';
 
 const DB_LOGIN = 'palletenjoer';
 const DB_PASSWORD = 87654321;
-const PORT = process.env.PORT ?? 10000;
+const PORT = 10000;
 
 const DB_URL = `mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@cluster0.afhplie.mongodb.net/Palletport?retryWrites=true&w=majority`
 
@@ -30,7 +30,7 @@ const startServer = async () => {
     try {
         mongoose.set('strictQuery', true);
         mongoose.connect(DB_URL);
-        app.listen(PORT, () => console.log('It work WOW!'));
+        app.listen(PORT, '0.0.0.0',() => console.log('It work WOW!'));
     } catch (error) {
         console.error(error);
     }
